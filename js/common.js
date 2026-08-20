64c2517959c18e0aaba885321f0abb0fcc74dada
9f5v4mzdqd-dot <masamiu._.u56513@icloud.com>
2026-08-20 12:43:50 +0900

    カンマ抜け修正

--- js/common.js
+++ js/common.js
@@ -43,7 +43,7 @@
     { key: "gaiyou", label: "市の概要", href: "about/gaiyou.html" },
     { key: "about", label: "市政情報", href: "about/shisei-joho.html" },
     { key: "piconu-chat", label: "ピコぬくんとチャット", href: "about/piconu-chat.html" },
-    { key: "radio", label: "FMピコぬ", href: "about/fm-piconu.html" }
+    { key: "radio", label: "FMピコぬ", href: "about/fm-piconu.html" },
     { key: "cable-tv", label: "ケーブルテレビ番組表", href: "about/cable-tv.html" }
   ];
 
